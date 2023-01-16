e4a946c213219b048e63662a4b62dfe6d73b4429
Pablo George <pablogeokar@hotmail.com>
2023-01-16 18:01:03 -0300

    v.0.0.1 INSTALAÇÃO LIMPA NEXTJS 13

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,13 @@
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
