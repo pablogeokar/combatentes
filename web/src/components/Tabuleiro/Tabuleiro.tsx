a1f896f5d2adda3a140e8874c7231d94d48bb552
Pablo George <pablogeokar@hotmail.com>
2023-01-19 16:38:59 -0300

    ajustes no tabuleiro, libera as casas apenas para o jogador 1, para arrumar as peças iniciais

--- web/src/components/Tabuleiro/Tabuleiro.tsx
+++ web/src/components/Tabuleiro/Tabuleiro.tsx
@@ -4,6 +4,19 @@ export default function Tabuleiro() {
   const casas = []
   const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
   const lagos = ['C5', 'C6', 'D5', 'D6', 'G5', 'G6', 'H5', 'H6']
+  const player1 = [
+    'A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1', 'J1',
+    'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2', 'J2',
+    'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3', 'I3', 'J3',
+    'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4', 'I4', 'J4'
+  ]
+  const player2 = [
+    'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7', 'I7', 'J7',
+    'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8', 'I8', 'J8',
+    'A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9', 'I9', 'J9',
+    'A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10', 'I10', 'J10',
+  ]
+
 
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < letras.length; j++) {
@@ -68,7 +81,11 @@ export default function Tabuleiro() {
             <div
               key={casa}
               id={casa}
-              className={`${styles.casa} ${lagos.includes(casa) ? styles.lago : ''}`} />
+              className={`
+              ${styles.casa} 
+              ${lagos.includes(casa) ? styles.lago : ''} 
+              ${!player1.includes(casa) ? styles.inativo : ''}
+              `} />
           ))
         }
       </div>
