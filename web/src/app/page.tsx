25d31e9ea222783bfe51f88796467ba23012d83e
Pablo George <pablogeokar@hotmail.com>
2023-01-29 09:32:38 -0300

    atualizando a vcersão do nextjs

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,34 +1,43 @@
-'use client'
+"use client";
 
-import styles from './home.module.scss'
-import { signIn, useSession, signOut } from 'next-auth/react'
-import { GooglePlayLogo } from 'phosphor-react'
-import Salas from './../components/Salas/Salas';
-
+import styles from "./home.module.scss";
+import { signIn, useSession, signOut } from "next-auth/react";
+import { GooglePlayLogo } from "phosphor-react";
+import Salas from "./../components/Salas/Salas";
 
 export default function Home() {
-
-  const { data: session } = useSession()
+  const { data: session } = useSession();
 
   return (
     <div className={styles.page}>
-
       <Salas />
 
-      {session ?
+      {session ? (
         <div className={styles.usuario}>
           <div>
             <span>{session.user?.name}</span>
             <span onClick={() => signOut()}>Sair</span>
           </div>
-          <img src={session.user?.image ? session.user?.image as string : '/user.png'} alt={session.user?.name as string} onError={(e: any) => { e.target.onError = null; e.target.src = "/user.png" }} />
+          <img
+            src={
+              session.user?.image
+                ? (session.user?.image as string)
+                : "/user.png"
+            }
+            alt={session.user?.name as string}
+            onError={(e: any) => {
+              e.target.onError = null;
+              e.target.src = "/user.png";
+            }}
+          />
         </div>
-        :
-        <button className={styles.btnLogin} onClick={() => signIn('google')}>Login com Google <GooglePlayLogo /></button>
-      }
+      ) : (
+        <button className={styles.btnLogin} onClick={() => signIn("google")}>
+          Login com Google <GooglePlayLogo />
+        </button>
+      )}
 
       <div className={styles.footer} />
-
     </div>
-  )
+  );
 }
