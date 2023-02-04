e84b3d43afc3705a22b233ea409ebda3bc6fb89d
Pablo George <pablogeokar@hotmail.com>
2023-02-04 07:19:02 -0300

    server

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,34 +1,59 @@
 import { prisma } from "./lib/prisma";
 import { FastifyInstance } from "fastify";
-import { query } from "express";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/partidas", async (request) => {
+  // Listagem das partidas
+  app.get("/partida", async (request) => {
     const partidas = await prisma.partida.findMany();
 
     return { partidas };
   });
-  app.get("/partidas/:id", async (request) => {
+
+  // Cria uma nova partida
+  app.post("/partida", async (request) => {
+    const { jogador_1 }: any = request.body;
+
+    if (jogador_1) {
+      const partida = await prisma.partida.create({
+        data: {
+          jogador_1,
+        },
+      });
+
+      return { partida };
+    } else {
+      return { error: "Você não informou o seu nome!" };
+    }
+  });
+
+  // Busca os dados da partida
+  app.get("/partida/:id", async (request) => {
     const { id }: any = request.params;
 
-    const partidas = await prisma.partida.findFirst({
+    const partida = await prisma.partida.findFirst({
       where: {
         id,
       },
     });
 
-    return { partidas };
+    return { partida };
   });
 
-  app.post("/partidas", async (request) => {
-    const { jogador_1 }: any = request.body;
+  // Entra na partida conforme o id informado
+  app.post("/partida/:id/entrar", async (request) => {
+    const { id }: any = request.params;
+    const { jogador_2 }: any = request.body;
 
-    const partida = await prisma.partida.create({
-      data: {
-        jogador_1,
-      },
-    });
-
-    return { partida };
+    if (jogador_2) {
+      const partida = await prisma.partida.update({
+        where: {
+          id,
+        },
+        data: {
+          jogador_2,
+        },
+      });
+      return partida;
+    }
   });
 }
